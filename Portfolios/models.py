5355c5b910d10140068ad344cd3df96b563dd50f
IceCobe <jaalaba@gmail.com>
2022-06-18 20:35:58 -0400

    Initial Model Created

--- Portfolios/models.py
+++ Portfolios/models.py
@@ -1,3 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    name = models.CharField()
+
+class Stock(models.Model):
+    ticker = models.CharField()
+    name = models.CharField()
+    quantity = models.PositiveIntegerField()
+    value = models.DecimalField(decimal_places=2)
+    goal = models.DecimalField()
+    actual = models.DecimalField()
+
+    portfolio = models.ForeignKey(Portfolio, on_delete=CASCADE)
+
