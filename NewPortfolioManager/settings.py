4c6b7610bd705fe90d990439952cd7b5e9742b82
IceCobe <jaalaba@gmail.com>
2022-07-13 18:34:07 -0400

    End State CSS Files Missing??

--- NewPortfolioManager/settings.py
+++ NewPortfolioManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m*yu5$bh5cc1x7lyfdk-d-11*&xv2july93c3$b-s^dgxa8zk$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'NewPortfolioManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = 'static/'
+
+location /static/ {                              
+    alias /path/to/project/static/;  
+    expires modified +1w;                        
+}  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- Portfolios/views.py
+++ Portfolios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from Portfolios.models import *
+
+
+def home(request):
+    stocks = Stock.objects.all()
+    return render(request, 'home.html', {'stocks':stocks})
